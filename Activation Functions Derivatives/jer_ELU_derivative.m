9c0b07e445a464542112cf3d451a1b923fa77eb6
Jerome Anthony McCave <74293400+Jerome12-Mc@users.noreply.github.com>
2022-01-14 10:23:31 +0000

    Add files via upload

---
+++ Activation Functions Derivatives/jer_ELU_derivative.m
@@ -0,0 +1,15 @@
+%This function represents the derivative of the Leaky Rectified Linear Unit activation function
+
+function y = jer_ELU_derivative(x)
+  n=size(x,1);
+  f_val = zeros(n,1);
+  for k=1:n 
+       if x(k)>=0
+       y=1;
+    else
+       y=0.1*exp(x(k));
+       end
+       f_val(k:k,1:1) = y;
+  end
+  y=f_val;
+end
